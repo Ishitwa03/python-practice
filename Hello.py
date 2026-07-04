000d7290d0578141422ff1e54ed2848710779831
Ishitwa03 <154496732+Ishitwa03@users.noreply.github.com>
2026-07-04 13:10:53 +0530

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -1,3 +1,4 @@
 print("Hello, GitHub!")
-print("My name is Ishitwa.")
-print("This is my first GitHub project.")
+print("My name is Ishitwa Gusaini.")
+print("I am learning Python.")
+print("This is Day 2 of my GitHub journey.")
